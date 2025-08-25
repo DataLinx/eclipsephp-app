5dd6cd8f9a1ce1cf068b9ec7b019fc5e0902e350
Kilian Trunk <hunterteammovies@gmail.com>
2025-08-25 09:31:48 +0200

    feat(notifications): implement tenant-scoped broadcast notifications (EC-200)
    * feat(notifications): implement tenant-scoped broadcast notifications
    
    * refactor: move tenant related code into core package
    
    ---------
    
    Co-authored-by: Omer Sabic <omer@datalinx.si>

--- routes/channels.php
+++ routes/channels.php
@@ -3,6 +3,7 @@
 use Eclipse\Core\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('Eclipse.Core.Models.User.{id}', function (User $user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('Eclipse.Core.Models.User.{id}.tenant.{tenantId}', function (User $user, $id, $tenantId) {
+    return (int) $user->id === (int) $id
+        && $user->sites()->whereKey($tenantId)->exists();
 });
